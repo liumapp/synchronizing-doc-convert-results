08198eb34789e3dfc9170459726559df20fdf754
liumapp <liumapp.com@gmail.com>
2018-07-02 20:38:05 +0800

    add index view file

--- synch-ui/src/router/index.js
+++ synch-ui/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Index from '@/views/index'
 
 Vue.use(Router)
 
@@ -7,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index
     }
   ]
 })
---
+++ synch-ui/src/views/index.vue
@@ -0,0 +1,14 @@
+/**
+ * @file index.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/2/18
+ */
+<template>
+</template>
+<script>
+export default {
+  name: 'index'
+}
+</script>
