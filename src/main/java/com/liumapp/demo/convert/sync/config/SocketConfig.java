0d534063909e3aa29c43c4b75e6767199a325154
liumapp <liumapp.com@gmail.com>
2018-07-02 15:13:37 +0800

    add websocket config

---
+++ src/main/java/com/liumapp/demo/convert/sync/config/SocketConfig.java
@@ -0,0 +1,17 @@
+package com.liumapp.demo.convert.sync.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liumapp
+ * @file SocketConfig.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/2/18
+ */
+@Configuration
+public class SocketConfig {
+
+    
+
+}
