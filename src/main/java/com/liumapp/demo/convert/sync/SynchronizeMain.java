803ae5211d8aa22f8bd7d82e800b03d3fc588cdb
liumapp <liumapp.com@gmail.com>
2018-07-02 15:12:19 +0800

    add spring boot main class

---
+++ src/main/java/com/liumapp/demo/convert/sync/SynchronizeMain.java
@@ -0,0 +1,20 @@
+package com.liumapp.demo.convert.sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file SynchronizeMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/2/18
+ */
+@SpringBootApplication
+public class SynchronizeMain {
+
+    public static void main (String[] args) {
+        SpringApplication.run(SynchronizeMain.class, args);
+    }
+
+}
--- src/main/java/com/liumapp/demo/convert/sync/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.liumapp.demo.convert.sync;
