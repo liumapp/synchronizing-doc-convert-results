d7291aee3263e43025bbaf5ce5c94f14d8bf2ec3
liumapp <liumapp.com@gmail.com>
2018-07-03 09:52:17 +0800

    add three types components

---
+++ synch-ui/src/components/doc-upload.vue
@@ -0,0 +1,11 @@
+/**
+ * @file doc-upload.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/3/18
+ */
+<template>
+</template>
+<script>
+</script>
