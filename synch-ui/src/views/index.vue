7b545217685ee5e77e64f77643f2f2b8b1ec2083
liumapp <liumapp.com@gmail.com>
2018-07-03 09:46:18 +0800

    use steps

--- synch-ui/src/views/index.vue
+++ synch-ui/src/views/index.vue
@@ -6,6 +6,23 @@
  * @date 7/2/18
  */
 <template>
+  <div>
+    <Row>
+      <Col span="18" offset="3">
+      <Card>
+        <Steps :current="current">
+          <Step title="上传doc文档" content="form表单上传一个待转换的doc文档"></Step>
+          <Step title="转换过程中" content="通过socket等待转换结果"></Step>
+          <Step title="获取转换结果" content="可下载转换好的pdf文档"></Step>
+        </Steps>
+        <upload-form v-if="current == 0" @setFormData="setFormData" @next="nextStep"></upload-form>
+        <first-check v-if="current == 1" @next="nextStep" @prev="prevStep" :formData="formData"></first-check>
+        <second-check v-if="current == 2" @next="nextStep" @prev="prevStep"></second-check>
+        <result v-if="current == 3"></result>
+      </Card>
+      </Col>
+    </Row>
+  </div>
 </template>
 <script>
 export default {
