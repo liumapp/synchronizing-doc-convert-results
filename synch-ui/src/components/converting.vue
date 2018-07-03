ef60a78cafa32b86f2c43dd1fd320fed226ccd8e
liumapp <liumapp.com@gmail.com>
2018-07-03 11:15:50 +0800

    update doc upload page

--- synch-ui/src/components/converting.vue
+++ synch-ui/src/components/converting.vue
@@ -6,6 +6,7 @@
  * @date 7/3/18
  */
 <template>
+
 </template>
 <script>
 export default {
--- synch-ui/src/components/doc-upload.vue
+++ synch-ui/src/components/doc-upload.vue
@@ -7,15 +7,21 @@
  */
 <template>
   <div>
-    <Upload
-      multiple
-      type="drag"
-      action="//jsonplaceholder.typicode.com/posts/">
-      <div style="padding: 20px 0">
-        <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
-        <p>Click or drag files here to upload</p>
-      </div>
-    </Upload>
+    <br>
+    <Row>
+      <Col span="8" offset="8">
+        <Upload
+          multiple
+          type="drag"
+          action="//jsonplaceholder.typicode.com/posts/">
+          <div style="padding: 20px 0">
+            <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+            <p>Click or drag files here to upload</p>
+          </div>
+        </Upload>
+      </Col>
+    </Row>
+
   </div>
 </template>
 <script>
