5de95c2f79a04f4809ac12115a983736b37f47ba
liumapp <liumapp.com@gmail.com>
2018-07-04 14:58:19 +0800

    use online number socket server to count number

---
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/socket/OnlineNumberSocketServer.java
@@ -0,0 +1,20 @@
+package com.liumapp.demo.convert.sync.socket;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * @author liumapp
+ * @file OnlineNumberSocketServer.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/4/18
+ */
+@ServerEndpoint(value = "/onlineSocket")
+@Component
+public class OnlineNumberSocketServer {
+
+
+
+}
