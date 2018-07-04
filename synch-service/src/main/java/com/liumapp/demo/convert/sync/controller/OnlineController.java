2f59df340aef864832ddf255a69e2582239befd8
liumapp <liumapp.com@gmail.com>
2018-07-04 14:41:55 +0800

    add online controller to count online number

---
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/controller/OnlineController.java
@@ -0,0 +1,19 @@
+package com.liumapp.demo.convert.sync.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liumapp
+ * @file OnlineController.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/4/18
+ */
+@RestController
+@RequestMapping("online")
+public class OnlineController {
+
+
+
+}
