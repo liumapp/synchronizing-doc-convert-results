6c21390ead99e63b91ff3563d2908909fe1de5e0
liumapp <liumapp.com@gmail.com>
2018-07-04 15:13:04 +0800

    online number socket server done

--- synch-service/src/main/java/com/liumapp/demo/convert/sync/socket/OnlineNumberSocketServer.java
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/socket/OnlineNumberSocketServer.java
@@ -2,7 +2,12 @@ package com.liumapp.demo.convert.sync.socket;
 
 import org.springframework.stereotype.Component;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * @author liumapp
@@ -15,6 +20,39 @@ import javax.websocket.server.ServerEndpoint;
 @Component
 public class OnlineNumberSocketServer {
 
+    private static int onlineNumber = 0;
+
+    private static CopyOnWriteArraySet<OnlineNumberSocketServer> clientWebSet = new CopyOnWriteArraySet<OnlineNumberSocketServer>();
+
+    private Session session;
+
+    @OnOpen
+    public void onOpen (Session session) {
+        this.session = session;
+        clientWebSet.add(this);
+        addOnlineNumber();
+    }
+
+    @OnClose
+    public void onClose () {
+        clientWebSet.remove(this);
+        subOnlineNumber();
+    }
+
+    @OnMessage
+    public void onMessage (String msg, Session session) {
+        for (OnlineNumberSocketServer client: clientWebSet) {
+            this.session.getAsyncRemote().sendText("hello from server");
+        }
+    }
+
+    public synchronized int addOnlineNumber () {
+        return OnlineNumberSocketServer.onlineNumber++;
+    }
+
+    public synchronized int subOnlineNumber () {
+        return OnlineNumberSocketServer.onlineNumber--;
+    }
 
 
 }
