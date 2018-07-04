d6803a1f39f8cd944a216380caabd68c3d4c7691
liumapp <liumapp.com@gmail.com>
2018-07-04 14:47:42 +0800

    update import package

--- synch-service/src/main/java/com/liumapp/demo/convert/sync/util/HttpUtil.java
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/util/HttpUtil.java
@@ -1,7 +1,37 @@
 package com.liumapp.demo.convert.sync.util;
 
-import org.springframework.stereotype.Component;
 
+import org.springframework.stereotype.Component;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 /**
  * @author liumapp
  * @file HttpUtil.java
