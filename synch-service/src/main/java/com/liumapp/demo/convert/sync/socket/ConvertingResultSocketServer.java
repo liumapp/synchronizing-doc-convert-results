500f54885f4e28bb81c3cf3a4c2c545b22f8e033
liumapp <liumapp.com@gmail.com>
2018-07-05 14:29:27 +0800

    add converting result socket server

---
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/socket/ConvertingResultSocketServer.java
@@ -0,0 +1,20 @@
+package com.liumapp.demo.convert.sync.socket;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * @author liumapp
+ * @file ConvertingResultSocketServer.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/5/18
+ */
+@ServerEndpoint(value = "convertingSocket")
+@Component
+public class ConvertingResultSocketServer {
+
+    
+
+}
