326e42fd224b3c8ce5d9eefd281a9fb4774bffb6
liumapp <liumapp.com@gmail.com>
2018-07-05 17:11:39 +0800

    ready to connect server and ui by convertId

--- synch-service/src/main/java/com/liumapp/demo/convert/sync/socket/ConvertingResultSocketServer.java
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/socket/ConvertingResultSocketServer.java
@@ -10,7 +10,6 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
