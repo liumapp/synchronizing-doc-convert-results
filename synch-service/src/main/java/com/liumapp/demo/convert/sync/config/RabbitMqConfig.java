0281f7440866f6e88accf64245d778a54a4a9323
liumapp <liumapp.com@gmail.com>
2018-07-06 09:45:45 +0800

    add rabbitmq require

---
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/config/RabbitMqConfig.java
@@ -0,0 +1,17 @@
+package com.liumapp.demo.convert.sync.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liumapp
+ * @file RabbitMqConfig.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/6/18
+ */
+@Configuration
+public class RabbitMqConfig {
+
+
+
+}
