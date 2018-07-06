b6aa76e165be4e5fdaf4e3c885938b17d9b1cd4e
liumapp <liumapp.com@gmail.com>
2018-07-06 17:05:38 +0800

    rm unnecessay things

--- synch-ui/src/views/index.vue
+++ synch-ui/src/views/index.vue
@@ -13,11 +13,9 @@
         <Steps :current="current">
           <Step title="上传doc文档" content="form表单上传一个待转换的doc文档"></Step>
           <Step title="转换过程中" content="通过socket等待转换结果，成功后获取转换好的pdf文档"></Step>
-          <Step title="结束"></Step>
         </Steps>
         <doc-upload v-if="current == 0" @next="nextStep" :convertId="convertId"></doc-upload>
         <converting v-if="current == 1" @next="nextStep" :convertId="convertId"></converting>
-        <convert-result v-if="current == 2"></convert-result>
       </Card>
       </Col>
     </Row>
@@ -32,11 +30,10 @@
 import util from '@/libs/util'
 import docUpload from '@/components/doc-upload'
 import converting from '@/components/converting'
-import convertResult from '@/components/convert-result'
 export default {
   name: 'index',
   components: {
-    docUpload, converting, convertResult
+    docUpload, converting
   },
   data () {
     return {
