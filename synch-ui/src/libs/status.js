19eee06e198b88fe8cb0109763cafa72174342f7
liumapp <liumapp.com@gmail.com>
2018-07-08 16:13:48 +0800

    add status to config params

---
+++ synch-ui/src/libs/status.js
@@ -0,0 +1,17 @@
+/**
+ * @file status.js
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/8/18
+ */
+
+let status = {};
+
+status.WAITING_CONVERT = 0;
+status.CONVERTING = 2;
+status.CONVERTED_SUCCESS = 1;
+
+export default status;
+
+
