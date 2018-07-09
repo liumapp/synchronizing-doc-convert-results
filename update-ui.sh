3a0cc80d806060cbacd110d6af6ae8c75cf31e0f
liumapp <liumapp.com@gmail.com>
2018-07-09 15:00:03 +0800

    update-ui shell done

--- update-ui.sh
+++ update-ui.sh
@@ -12,5 +12,14 @@ echo '$                                                           $'
 echo '============================================================='
 echo '.'
 
+rm -rf ./synch-service/src/main/resources/static/static
+
+rm ./synch-service/src/main/resources/templates/index.html
+
+cp -r ./synch-ui/dist/static ./synch-service/src/main/resources/static/
+
+cp ./synch-ui/dist/index.html ./synch-service/src/main/resources/templates/index.html
+
+chmod -R a+w ../synchronizing-doc-convert-results
 
 
