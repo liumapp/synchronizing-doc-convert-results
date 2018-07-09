dd2c61bbb3a093cc32dfaae036183293ed812b72
liumapp <liumapp.com@gmail.com>
2018-07-09 14:26:15 +0800

    ready to add docker image shell

---
+++ build-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      liumapp                              $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  email:    liumapp.com@gmail.com                          $'
+echo '$  homePage: http://www.liumapp.com                         $'
+echo '$  Github:   https://github.com/liumapp                     $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
