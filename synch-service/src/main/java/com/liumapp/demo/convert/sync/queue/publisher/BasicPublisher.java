4d936ef47a4e237d47591674ad6722730ed50ad7
liumapp <liumapp.com@gmail.com>
2018-07-13 17:10:31 +0800

    need set message post processor in basic publisher

--- synch-service/src/main/java/com/liumapp/demo/convert/sync/queue/publisher/BasicPublisher.java
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/queue/publisher/BasicPublisher.java
@@ -1,7 +1,16 @@
 package com.liumapp.demo.convert.sync.queue.publisher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate.ConfirmCallback;
 import org.springframework.amqp.rabbit.support.CorrelationData;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.annotation.Resource;
 
 /**
  * @author liumapp
@@ -12,8 +21,30 @@ import org.springframework.amqp.rabbit.support.CorrelationData;
  */
 public abstract class BasicPublisher implements ConfirmCallback {
 
-    @Override
-    public void confirm(CorrelationData correlationData, boolean b, String s) {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Resource
+    public RabbitTemplate rabbitTemplate;
+
+    @Value("${spring.rabbitmq.custom.routingkey}")
+    private String routingKey;
+
+    @Value("${spring.rabbitmq.custom.appid}")
+    private String appId;
+
+    public void sendMessage (final String serviceName, final String serviceMethodName, final String correlationId, Object request) {
+        logger.info("----- send msg : [this.{}, serviceMethodName:{} serviceName:{} correlationId: {}]", this.getClass(), serviceMethodName, serviceName, correlationId);
+        rabbitTemplate.setConfirmCallback(this);
+        rabbitTemplate.setCorrelationKey(correlationId);
+        rabbitTemplate.convertAndSend(routingKey, request, new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+
+                return null;
+            }
+        }, new CorrelationData(correlationId));
     }
+
+    @Override
+    public abstract void confirm(CorrelationData correlationData, boolean ack, String cause);
 }
