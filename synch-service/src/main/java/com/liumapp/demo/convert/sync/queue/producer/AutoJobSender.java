7a98fb6c95676b114a89fc96c734247424138eb7
liumapp <liumapp.com@gmail.com>
2018-07-13 14:33:43 +0800

    add auto job sender

--- synch-service/src/main/java/com/liumapp/demo/convert/sync/queue/producer/AutoJobSender.java
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/queue/producer/AutoJobSender.java
@@ -1,40 +1,34 @@
 package com.liumapp.demo.convert.sync.queue.producer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author liumapp
- * @file AddConvertDocJob.java
+ * @file AutoJobSender.java
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
- * @date 7/6/18
+ * @date 7/13/18
  */
 @Component
-public class ConvertDocJobSender implements RabbitTemplate.ReturnCallback, RabbitTemplate.ConfirmCallback, InitializingBean {
-
-    private static Logger logger = LoggerFactory.getLogger(ConvertDocJobSender.class);
-
-    @Autowired
-    private AmqpTemplate amqpTemplate;
+public class AutoJobSender implements RabbitTemplate.ReturnCallback, RabbitTemplate.ConfirmCallback, InitializingBean{
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     private AtomicInteger atomicInteger = new AtomicInteger(0);
 
-    public void send (String jsonPattern) {
-        logger.info("add new convert job, doc path is : " + jsonPattern);
-        this.amqpTemplate.convertAndSend("doc-convert-queue" , jsonPattern);
+    @Scheduled(cron = "* /5 * * * * *")
+    public void printer () {
+        System.out.println("send job per 5 seconds: " + atomicInteger.get());
+        atomicInteger = new AtomicInteger(0);
     }
 
     @Override
