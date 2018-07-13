bdb4cd21e068183e8f1d300e39e67e452a84ae08
liumapp <liumapp.com@gmail.com>
2018-07-13 14:40:55 +0800

    complete send method and returnedMessage

--- synch-service/src/main/java/com/liumapp/demo/convert/sync/queue/producer/AutoJobSender.java
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/queue/producer/AutoJobSender.java
@@ -1,6 +1,10 @@
 package com.liumapp.demo.convert.sync.queue.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.InitializingBean;
@@ -8,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -25,20 +30,33 @@ public class AutoJobSender implements RabbitTemplate.ReturnCallback, RabbitTempl
 
     private AtomicInteger atomicInteger = new AtomicInteger(0);
 
+    private static Logger logger = LoggerFactory.getLogger(AutoJobSender.class);
+
     @Scheduled(cron = "* /5 * * * * *")
     public void printer () {
         System.out.println("send job per 5 seconds: " + atomicInteger.get());
         atomicInteger = new AtomicInteger(0);
     }
 
-    @Override
-    public void confirm(CorrelationData correlationData, boolean b, String s) {
-
+    public void send (String exchange, String routingKey, String msg, CorrelationData correlationData) {
+        atomicInteger.incrementAndGet();
+        Message message = MessageBuilder.withBody(msg.getBytes())
+                .setContentType(MessageProperties.CONTENT_TYPE_JSON)
+                .build();
+        rabbitTemplate.convertAndSend(exchange, routingKey, message, correlationData);
     }
 
     @Override
-    public void returnedMessage(Message message, int i, String s, String s1, String s2) {
+    public void confirm(CorrelationData correlationData, boolean b, String s) {
+    }
 
+    /**
+     * 当消息发送出去找不到对应路由队列时，将会把消息退回
+     * 如果有任何一个路由队列接收投递消息成功，则不会退回消息
+     */
+    @Override
+    public void returnedMessage(Message message, int i, String s, String s1, String s2) {
+        logger.error("auto job sender failed: " + Arrays.toString(message.getBody()));
     }
 
     @Override
