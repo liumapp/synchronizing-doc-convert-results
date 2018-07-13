e0458957352f2583a675002d7a1c835ffc3d5f20
liumapp <liumapp.com@gmail.com>
2018-07-13 16:04:04 +0800

    add listener aware for rabbitmq

---
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/aware/RabbitMqListenerAware.java
@@ -0,0 +1,11 @@
+package com.liumapp.demo.convert.sync.aware;
+
+/**
+ * @author liumapp
+ * @file RabbitMqListenerAware.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/13/18
+ */
+public class RabbitMqListenerAware {
+}
