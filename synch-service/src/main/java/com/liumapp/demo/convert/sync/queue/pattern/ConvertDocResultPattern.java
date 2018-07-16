7718db2e4f823e1280bb3930ae3910113c01ed80
liumapp <liumapp.com@gmail.com>
2018-07-16 14:05:58 +0800

    add convert doc result pattern

---
+++ synch-service/src/main/java/com/liumapp/demo/convert/sync/queue/pattern/ConvertDocResultPattern.java
@@ -0,0 +1,18 @@
+package com.liumapp.demo.convert.sync.queue.pattern;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author liumapp
+ * @file ConvertDocResultPattern.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/16/18
+ */
+@Component
+public class ConvertDocResultPattern {
+
+
+
+
+}
